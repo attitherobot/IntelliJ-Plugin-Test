845aa3058e72cf34291fcfdaa60c72ed0aebe32e
GitHub Action <action@github.com>
2021-09-22 14:07:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/attitherobot/intellijplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.attitherobot.intellijplugintest.services
+
+import com.github.attitherobot.intellijplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/attitherobot/intellijplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/attitherobot/intellijplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.attitherobot.intellijplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.attitherobot.intellijplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
